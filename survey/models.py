9018fcbcc40f15c347c742da89785ca5d1ecfe8d
Sean Goedecke <seangoedecke@gmail.com>
2016-05-14 16:33:34 +1000

    Consensus app, first commit

---
+++ survey/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Claim(models.Model):
+	author = models.ForeignKey('auth.User')
+	text = models.CharField(max_length=600)
+	created_date = models.DateTimeField(default=timezone.now)
+	yeas = models.IntegerField()
+	nays = models.IntegerField()
+	upvotes = models.IntegerField()
+	downvotes = models.IntegerField()
+	
+	def publish(self):
+		self.save()
+		
+	def __str__(self):
+		return self.text
